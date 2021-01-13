8b064de391940e2b5d7c948865076472351d639a
LZhizhou <luzhizhou@hotmail.com>
2021-01-13 13:25:41 -0700

    Initial Commit

---
+++ Shared/remember_reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  remember_reminderApp.swift
+//  Shared
+//
+//  Created by Zhizhou Lu on 2021-01-13.
+//
+
+import SwiftUI
+
+@main
+struct remember_reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
